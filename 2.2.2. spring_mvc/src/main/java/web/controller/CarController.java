c0d6750a782f9c32a878713ee30e679c518081a7
aanaever <nasty.ever18@gmail.com>
2021-02-04 15:45:54 +0300

    gb

--- 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
@@ -3,6 +3,7 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarServiceImpl;
 
@@ -13,7 +14,7 @@ public class CarController {
     CarServiceImpl service = new CarServiceImpl();
 
     @GetMapping(value = "/cars")
-    public String printCar(Integer count, Model model) {
+    public String printCar(@RequestParam Integer count, Model model) {
         if (count == null) {
             List<Car> allCars = service.getCarList();
             model.addAttribute("cars", allCars);
